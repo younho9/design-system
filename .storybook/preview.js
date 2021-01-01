5050f72de2449065240325a0056069f968e6e43f
younho9 <younho9.choo@gmail.com>
2021-01-01 18:46:33 +0900

    style: Apply global style and fonts to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,17 @@
+import { GlobalStyle } from '../src/styles/global';
+import { loadFontsForStorybook } from '../src/utils';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' }
+};
+
+export const decorators = [
+  (Story) => (
+    <>
+      <GlobalStyle />
+      <Story />
+    </>
+  )
+];
+
+loadFontsForStorybook();
